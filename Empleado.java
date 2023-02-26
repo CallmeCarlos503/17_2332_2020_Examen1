150e7a49275db32ba60b0a8ba5320b65129bdc6c
Carlos_Alberto_Sanchez <95440504+CallmeCarlos503@users.noreply.github.com>
2023-02-26 11:37:03 -0600

    first commit

---
+++ Empleado.java
@@ -0,0 +1,37 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Empleado {
+    Scanner leer = new Scanner(System.in);
+    String Nombre, Apellido, Puesto;
+    int Edad;
+    double salario;
+    DecimalFormat formato = new DecimalFormat("###.##");
+
+    public Empleado() {
+        System.out.println("*****BIENVENIDO AL SISTEMA DE EMPLEADOS*****");
+    }
+
+    public void lectura() {
+        System.out.println("Escriba el nombre del empleado");
+        Nombre = leer.nextLine();
+        System.out.println("Escriba el apellido del empleado");
+        Apellido = leer.nextLine();
+        System.out.println("Escriba el puesto del empleado");
+        Puesto = leer.nextLine();
+        System.out.println("Escriba la edad del empleado");
+        Edad = leer.nextInt();
+        System.out.println("Escriba el salario del empleado");
+        salario = leer.nextDouble();
+
+    }
+
+    public void mensaje() {
+        System.out.println("Nombre completo: " + Nombre
+                + " " + Apellido +
+                "\n Edad: " + Edad +
+                "\n Puesto: " + Puesto
+                + "\n Salario base: $" + formato.format(salario));
+    }
+
+}
---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        Empleado em= new Empleado();
+        em.lectura();
+        em.mensaje();
+    }
+}
